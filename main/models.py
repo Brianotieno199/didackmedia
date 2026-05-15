ae0df75297b9ed95a801550d4fb003afbf561ab8
Derick Ochieng <ochiengderick12@gmail.com>
2026-05-16 01:49:32 +0300

    fixing image loading issue

--- main/models.py
+++ main/models.py
@@ -1,10 +1,45 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+import os
+
+
+def optimize_image(image_field, max_width=1200, quality=75):
+    if not image_field:
+        return
+
+    img = Image.open(image_field)
+
+    if img.mode in ("RGBA", "P"):
+        img = img.convert("RGB")
+
+    width, height = img.size
+
+    if width > max_width:
+        new_height = int((max_width / width) * height)
+        img = img.resize((max_width, new_height), Image.LANCZOS)
+
+    buffer = BytesIO()
+    img.save(buffer, format="WEBP", quality=quality, optimize=True)
+
+    file_name = os.path.splitext(image_field.name)[0] + ".webp"
+
+    image_field.save(
+        file_name,
+        ContentFile(buffer.getvalue()),
+        save=False
+    )
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     cover_image = models.ImageField(upload_to='categories/')
+
+    def save(self, *args, **kwargs):
+        optimize_image(self.cover_image, max_width=1400, quality=75)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -15,6 +50,10 @@ class Photo(models.Model):
     image = models.ImageField(upload_to='gallery/')
     title = models.CharField(max_length=100, blank=True)
 
+    def save(self, *args, **kwargs):
+        optimize_image(self.image, max_width=1200, quality=75)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title or "Photo"
 
@@ -24,7 +63,11 @@ class Blog(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='blog/')
     created = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0)  # ✅ like system
+    likes = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        optimize_image(self.image, max_width=1200, quality=75)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -32,15 +75,16 @@ class Blog(models.Model):
 
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)  # ✅ replies
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     message = models.TextField()
-    approved = models.BooleanField(default=False)  # ✅ moderation
+    approved = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class ContactMessage(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True)
